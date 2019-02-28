68d9d3f5b06cf8e5bfe737c5383349a0e2979021
hkdemircan <hkdemircan06@gmail.com>
2019-03-01 00:19:03 +0300

    initialize Spring boot starter, add dependency

---
+++ petclinc/src/main/java/com/hkdemircan/petclinc/PetclincApplication.java
@@ -0,0 +1,13 @@
+package com.hkdemircan.petclinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclincApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetclincApplication.class, args);
+    }
+
+}
