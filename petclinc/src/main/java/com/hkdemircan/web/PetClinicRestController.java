a27afa9bedd5e96d75bb679639dfdc806283f2f9
hkdemircan <hkdemircan06@gmail.com>
2019-03-04 14:48:25 +0300

    rest crud işlemler ve testleri tamamlandı.

--- petclinc/src/main/java/com/hkdemircan/web/PetClinicRestController.java
+++ petclinc/src/main/java/com/hkdemircan/web/PetClinicRestController.java
@@ -4,9 +4,12 @@ import com.hkdemircan.exception.OwnerNotFoundException;
 import com.hkdemircan.model.Owner;
 import com.hkdemircan.service.PetClinicService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -15,6 +18,45 @@ public class PetClinicRestController {
     @Autowired
     private PetClinicService petClinicService;
 
+    @RequestMapping(method = RequestMethod.DELETE, value = "/owner/{id}")
+    public ResponseEntity<?> deleteOwner(@PathVariable("id") Long id){
+        try{
+            petClinicService.deleteOwner(id);
+            return ResponseEntity.ok().build();
+        }catch (OwnerNotFoundException ex){
+            return ResponseEntity.notFound().build();
+        }catch (Exception ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/owner/{id}")
+    public ResponseEntity<?> updateOwner(@PathVariable Long id, @RequestBody Owner ownerRequest){
+        try{
+            Owner owner = petClinicService.findOwner(id);
+            owner.setFirstName(ownerRequest.getFirstName());
+            owner.setLastName(ownerRequest.getLastName());
+            petClinicService.updateOwner(owner);
+            return ResponseEntity.ok().build();
+        }catch (OwnerNotFoundException ex){
+            return ResponseEntity.notFound().build();
+        }catch (Exception ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+    }
+    @RequestMapping(method = RequestMethod.POST, value = "/owner" )
+    public ResponseEntity<URI> createOwner(@RequestBody Owner owner){
+
+        try {
+            petClinicService.createOwner(owner);
+            Long id = owner.getId();
+            URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(id).toUri();
+            return ResponseEntity.created(location).build();
+        }catch (Exception ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
     @RequestMapping(method = RequestMethod.GET, value = "/owners")
     public ResponseEntity<List<Owner>> getOwners(){
        List<Owner> owners = petClinicService.findOwners();
--- petclinc/src/test/java/com/hkdemircan/petclinc/PetClinicRestControllerTest.java
+++ petclinc/src/test/java/com/hkdemircan/petclinc/PetClinicRestControllerTest.java
@@ -3,11 +3,14 @@ package com.hkdemircan.petclinc;
 import com.hkdemircan.model.Owner;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -48,5 +51,42 @@ public class PetClinicRestControllerTest {
         List<Map<String, String>> body = responseEntity.getBody();
         List<String> firstNames = body.stream().map(e->e.get("firstName")).collect(Collectors.toList());
         MatcherAssert.assertThat(firstNames, Matchers.containsInAnyOrder("kadir","hasan"));
+    }
+
+    @Test
+    public void testCreateOwner(){
+        Owner owner = new Owner();
+        owner.setFirstName("deneme");
+        owner.setLastName("dene");
+
+        URI location = restTemplate.postForLocation("http://localhost:8080/rest/owner", owner);
+        Owner owner2 = restTemplate.getForObject(location,Owner.class);
+        MatcherAssert.assertThat(owner2.getFirstName(), Matchers.equalTo(owner.getFirstName()));
+        MatcherAssert.assertThat(owner2.getLastName(), Matchers.equalTo(owner.getLastName()));
+    }
+
+    @Test
+    public void testUpdateOwner(){
+        Owner owner = restTemplate.getForObject("http://localhost:8080/rest/owner/1", Owner.class);
+
+        MatcherAssert.assertThat(owner.getFirstName(), Matchers.equalTo("kadir"));
+        owner.setFirstName("nem");
+        restTemplate.put("http://localhost:8080/rest/owner/1", owner);
+
+        owner = restTemplate.getForObject("http://localhost:8080/rest/owner/1", Owner.class);
+        MatcherAssert.assertThat(owner.getFirstName(),Matchers.equalTo("nem"));
+    }
+
+    @Test
+    public void testDeleteOwner(){
+        restTemplate.delete("http://localhost:8080/rest/owner/1");
+        try{
+            ResponseEntity<Owner> owner = restTemplate.getForEntity("http://localhost:8080/rest/owner/1", Owner.class);
+            Assert.fail("should have not returned owner");
+        }catch (RestClientException ex){
+
+        }
+
+
     }
 }
